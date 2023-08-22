f4e95202406a97f5b43b4c93f05b454b4d12634c
Phi Van Duc <phivanduc325@gmail.com>
2023-08-23 00:58:42 +0700

    Fix Day21

--- Day-21-JS/Exercise-4/script.js
+++ Day-21-JS/Exercise-4/script.js
@@ -1,53 +1,24 @@
 var arr = [2, 4, 6, 8, 10, 12];
-
-// var maxNumber = arr.reduce(function (pre, curr) {
-//     return pre < curr ? curr : pre;
-// });
-
-// var total = arr.reduce(function (pre, curr) {
-//     return pre + curr;
-// }, 0);
-
-// console.log(maxNumber);
-// console.log(total);
-
-// Mảng có kích thước bằng 0 - không có giá trị khởi tạo
-// Mảng có kích thước bằng 1 - Không có giá trị khởi tạo
-// Có giá trị khởi tạo
-// Giá trị khởi tạo bằng chính phần tử đầu tiên của mảng
+var arr2 = [];
 
 Array.prototype.reduce2 = function (callback, initialValue) {
     let array = this;
+    let index = 0;
 
-    if (Array.isArray(array)) {
-        let index = 0;
-        let result;
-
-        if (initialValue === undefined && array.length === 0) {
-            return "Error";
-        }
-        else if (initialValue === undefined && array.length > 0) {
-            index = 1;
-            initialValue = array[0];
-        }
-
-        do {
-            if (index === array.length - 1) {
-                result = callback(initialValue, array[index], index, array);
-            }
-            else {
-                initialValue = callback(initialValue, array[index], index, array);
-                result = initialValue;
-            }
-            ++index;
-        }
-        while (index < array.length);
-
-        return result;
+    if (initialValue === undefined && array.length === 0) {
+        return "Error";
     }
-    else {
-        return "Không hợp lệ";
+    else if (initialValue === undefined && array.length > 0) {
+        index = 1;
+        initialValue = array[0];
     }
+
+    while (index < array.length) {
+        initialValue = callback(initialValue, array[index], index, array);
+        ++index;
+    }
+
+    return initialValue;
 }
 
 
@@ -66,5 +37,10 @@ var total2 = arr.reduce2(function (pre, curr) {
     return pre + curr;
 }, 0);
 
+var total = arr2.reduce(function (pre, curr) {
+    return pre + curr;
+}, 0);
+
 console.log(maxNumber2);
 console.log(total2);
+console.log(total);
