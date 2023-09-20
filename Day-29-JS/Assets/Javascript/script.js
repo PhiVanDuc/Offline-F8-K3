1c59c1681f404aac005c6e69e91463ee3975c61c
Phi Van Duc <phivanduc325@gmail.com>
2023-09-21 02:04:43 +0700

    Final Day-29-JS

--- Day-29-JS/Assets/Javascript/script.js
+++ Day-29-JS/Assets/Javascript/script.js
@@ -1,7 +1,6 @@
 const listCourses = document.querySelector(".list-courses");
 const listItems = listCourses.querySelectorAll(".list-item");
 
-
 listItems.forEach(function (element) {
     element.addEventListener("dragstart", function () {
         element.classList.add("dragging");
@@ -12,24 +11,30 @@ listItems.forEach(function (element) {
     });
 });
 
-
 listCourses.addEventListener("dragover", function (e) {
     e.preventDefault();
     const dragging = listCourses.querySelector(".dragging");
     const remainingElements = listCourses.querySelectorAll(".list-item:not(.dragging)");
 
-    const changeElement = Array.from(remainingElements).reduce(function (closest, child) {
-        const box = child.getBoundingClientRect();
-        const offset = e.clientY - box.top - box.height / 2;
+    let changeUp = Array.from(remainingElements).find(function (element) {
+        const box = element.getBoundingClientRect();
+        if (e.clientY >= box.top - (box.height / 2))
+            return element;
+    });
 
-        if (offset < 0 && offset > closest.offset)
-            return { offset: offset, element: child };
-        else
-            return closest;
-    }, { offset: Number.NEGATIVE_INFINITY }).element;
+    let changeDown = Array.from(remainingElements).find(function (element) {
+        const box = element.getBoundingClientRect();
+        if (e.clientY <= box.top + (box.height / 2))
+            return element;
+    });
 
-    if (changeElement)
-        listCourses.insertBefore(dragging, changeElement);
+    if (changeUp)
+        listCourses.insertBefore(dragging, changeUp);
+    else
+        listCourses.prepend(dragging);
+
+    if (changeDown)
+        listCourses.insertBefore(dragging, changeDown);
     else
         listCourses.appendChild(dragging);
 });
