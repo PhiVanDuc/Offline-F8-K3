0bbd81d6d55be900c631c58184cfe063302eb2bc
Phi Van Duc <phivanduc325@gmail.com>
2023-10-05 01:49:53 +0700

    new Update Day-33

--- Day-33-JS/script.js
+++ Day-33-JS/script.js
@@ -9,9 +9,19 @@ recognition.maxAlternatives = 1;
 
 
 const speechBtn = document.querySelector(".speech-btn");
+const regexWay = /^(tới|đường tới|đến|đường đến|đi đến|chỉ đường|chỉ đường tới|chỉ đường đến).*$/;
+const regexMusic = /^(bật bài|mở bài|bật bài hát|mở bài hát|bài hát|nghe bài|nghe bài hát|phát nhạc|phát bài|nghe nhạc).*$/;
+const regexVideo = /^(video|mở video|phát video|bật video|xem video).*$/;
+let showOrHidden = false;
 speechBtn.addEventListener("click", function () {
     recognition.start();
 });
+
+
+function openNewTab(regex, string, url) {
+    let keyword = string.replace(string.match(regex)[1], "").trim().replace(/\s+/, "+");
+    window.open(`${url}${keyword}`, "_blank");
+}
 
 
 recognition.addEventListener("result", function (event) {
@@ -33,8 +43,14 @@ recognition.addEventListener("result", function (event) {
         case "google maps": case "bản đồ":
             window.open("https://www.google.com/maps/place", "_blank");
             break;
-        case lowerText.match(/(chỉ đường|chỉ đường tới|tới|đường tới).*/).input:
-            console.log(lowerText.match(/^(?!.*(chỉ đường|chỉ đường tới|tới|đường tới)).*$/i));
+        case lowerText.match(regexWay) ? lowerText.match(regexWay).input : false:
+            openNewTab(regexWay, lowerText, "https://www.google.com/maps/search/");
+            break;
+        case lowerText.match(regexMusic) ? lowerText.match(regexMusic).input : false:
+            openNewTab(regexMusic, lowerText, "https://zingmp3.vn/tim-kiem/tat-ca?q=");
+            break;
+        case lowerText.match(regexVideo) ? lowerText.match(regexVideo).input : false:
+            openNewTab(regexVideo, lowerText, "https://www.youtube.com/results?search_query=");
             break;
     }
 });
