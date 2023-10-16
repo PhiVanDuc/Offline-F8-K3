d11b8d12f9a67f9c538469e575e95195ab654a14
Phi Van Duc <phivanduc325@gmail.com>
2023-10-16 12:52:42 +0700

    Update Day-36-JS

--- Day-36-JS/Assets/Javascript/script.js
+++ Day-36-JS/Assets/Javascript/script.js
@@ -12,17 +12,20 @@ let htmlContainer = `
 </div>
 
 <div class="calc-point">
-    <div class="number-question">
-        <p><span>0</span>/<span>0</span></p>
+    <div class="wrap">
+        <div class="number-question">
+            <p><span>0</span>/<span>0</span></p>
+        </div>
+
+        <div class="strike-bar">
+            <div class="strike-progress"></div>
+            <p class="strike"></p>
+            <div class="strike-line-left"></div>
+            <div class="strike-line-right"></div>
+        </div>
     </div>
 
-    <div class="strike-bar">
-        <div class="strike-progress"></div>
-        <p class="strike"></p>
-        <div class="strike-line-left"></div>
-        <div class="strike-line-right"></div>
-        <p class="plus-point"></p>
-    </div>
+    <p class="total-point">0</p>
 </div>
 
 <div class="question">
@@ -30,7 +33,6 @@ let htmlContainer = `
 
 <div class="result"></div>
 `;
-
 let questions;
 let catchError = "";
 
@@ -48,7 +50,7 @@ async function getData() {
 getData();
 
 
-btnStart.addEventListener("click", function () {
+function handleQuizizz() {
     btnStart.style.display = "none";
     countdown.style.visibility = "visible";
 
@@ -60,25 +62,25 @@ btnStart.addEventListener("click", function () {
             countdown.style.visibility = "hidden";
             clearInterval(animationCountdown);
 
-            // Bắt đầu xử lý các logic khi đã chạy xong phần khởi động
 
+            // Bắt đầu xử lý các logic khi đã chạy xong phần khởi động
             // Thêm nội dung vào container
             container.innerHTML = htmlContainer;
             container.style.transform = "translateX(0%)";
 
-            // Xử lý các logic khác
             const progress = container.querySelector(".progress");
             const calcPoint = document.querySelector(".calc-point");
             const currentQuestion = calcPoint.querySelector(".number-question p span:first-child");
             const totalQuestion = calcPoint.querySelector(".number-question p span:last-child");
             const question = document.querySelector(".question");
             const result = document.querySelector(".result");
+            const strikeProgress = document.querySelector(".strike-progress");
+            const statistical = document.querySelector(".statistical");
 
             const checkQuestions = setInterval(function () {
                 // Nếu lấy được dữ liệu rồi thì mới xử lý logic và xóa setInterval đi
                 if (questions) {
                     clearInterval(checkQuestions);
-
                     currentQuestion.innerText = 1;
                     totalQuestion.innerText = questions.length;
 
@@ -97,10 +99,67 @@ btnStart.addEventListener("click", function () {
                     }
 
                     function rerenderQuestion() {
-                        if (+currentQuestion.innerText === questions.length) return;
+                        ++currentQuestion.innerText;
+                        if (+currentQuestion.innerHTML > questions.length) {
+                            const percent = (countCorrect / questions.length) * 100;
+                            let numberStreak = 0;
+                            strike.push(countStrike);
+                            strike.forEach(function (element) {
+                                if (numberStreak < element) numberStreak = element;
+                            });
+                            container.innerHTML = "";
+                            container.style.paddingTop = "0px";
+                            statistical.style.display = "flex";
+                            statistical.innerHTML = `
+                                <div class="accuracy">
+                                    <h4>Accuracy</h4>
+                                    <div class="accuracy-bar">
+                                        <div class="percent-correct">
+                                            <p class="percent">${percent}%</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="info">
+                                    <div class="box box-score">
+                                        <p class="number number-score">${score}</p>
+                                        <p>Score</p>
+                                    </div>
+
+                                    <div class="box box-streak">
+                                        <p class="number number-streak">${numberStreak}</p>
+                                        <p>Streak</p>
+                                    </div>
+
+                                    <div class="box box-correct">
+                                        <p class="number number-correct">${countCorrect}</p>
+                                        <p>Correct</p>
+                                    </div>
+
+                                    <div class="box box-incorrect">
+                                        <p class="number number-incorrect">${countIncorrect}</p>
+                                        <p>Incorrect</p>
+                                    </div>
+                                </div>
+
+                                <button class="btn-play-again">Play again</button>
+                            `;
+                            statistical.querySelector(".percent-correct").style.width = percent + "%";
+
+                            statistical.querySelector("button").addEventListener("click", function () {
+                                statistical.innerHTML = "";
+                                container.style.paddingTop = "5px";
+                                container.style.transform = "translate(-110%)";
+                                statistical.style.display = "none";
+                                btnStart.style.display = "block";
+                                saveChoose, strike = [];
+                                numberCorrect, score, countCorrect, countIncorrect, countStrike, calc = 0;
+                                numberCountdown.innerText = 3;
+                                numberCountdown.style.transform = "scale(0)";
+                            });
+                        }
                         else {
                             result.style.display = "none";
-                            ++currentQuestion.innerText;
                             progress.style.width = "100%";
                             startTime = performance.now();
                             renderQuestion(+currentQuestion.innerText);
@@ -130,18 +189,51 @@ btnStart.addEventListener("click", function () {
 
                         setTimeout(function () {
                             const number = progress.style.width.match(/^(\d+)/);
-                            if (+number[1] === 0 && +currentQuestion.innerText < questions.length) {
-                                rerenderQuestion();
+                            if (+number[1] === 0) {
                                 cancelAnimationFrame(currentAnimation);
+                                rerenderQuestion(+currentQuestion.innerText);
+                                strike.push(countStrike);
+                                countStrike = 0;
+                                updateStreakBar(countStrike);
                             }
-                            else if (+number[1] === 0 && +currentQuestion.innerText === questions.length) return;
                         }, duration);
                     }
                     renderQuestion(+currentQuestion.innerText);
 
+                    function updateStreakBar(countStrike) {
+                        if (countStrike === 0) {
+                            calc = 0
+                            document.querySelector(".strike-bar .strike").innerText = "";
+                        }
+                        else if (countStrike > 0) {
+                            document.querySelector(".strike-bar .strike").innerText = "Streak";
+                            if (countStrike === 1) {
+                                calc = (1 / 3) * 100;
+                                score += 100;
+                            }
+                            else if (countStrike === 2) {
+                                calc = (2 / 3) * 100;
+                                score += 200;
+                            }
+                            else if (countStrike >= 3) {
+                                calc = 100;
+                                score += 300;
+                            }
+                        }
+                        document.querySelector(".calc-point .total-point").innerText = score;
+                        strikeProgress.style.width = calc + "%";
+                    }
+
                     let saveChoose = [];
                     let numberCorrect = 0;
                     let canClick = true;
+
+                    let score = 0;
+                    let countCorrect = 0;
+                    let countIncorrect = 0;
+                    let countStrike = 0;
+                    let strike = [];
+                    let calc = 0;
 
                     question.addEventListener("click", function (e) {
                         const target = e.target;
@@ -158,16 +250,17 @@ btnStart.addEventListener("click", function () {
                                 if (target.parentElement.parentElement.dataset.choose === "1") {
                                     if (target.dataset.correct === "false") {
                                         target.style.backgroundColor = "#DC4C64";
-
                                         answers.forEach((answer) => {
                                             if (answer.dataset.correct === "true") {
                                                 answer.style.backgroundColor = "#14A44D";
                                                 return;
                                             }
                                         });
-
                                         Object.assign(result.style, css);
                                         result.innerText = "Incorrect!";
+                                        strike.push(countStrike);
+                                        countStrike = 0;
+                                        countIncorrect++;
 
                                         setTimeout(function () {
                                             rerenderQuestion();
@@ -180,6 +273,9 @@ btnStart.addEventListener("click", function () {
                                         css.backgroundColor = "#14A44D";
                                         Object.assign(result.style, css);
                                         result.innerText = "Correct!";
+                                        score += 200;
+                                        countStrike++;
+                                        countCorrect++;
 
                                         setTimeout(function () {
                                             rerenderQuestion();
@@ -207,6 +303,9 @@ btnStart.addEventListener("click", function () {
                                                 css.backgroundColor = "#14A44D";
                                                 Object.assign(result.style, css);
                                                 result.innerText = "Correct!";
+                                                score += 300;
+                                                countStrike++;
+                                                countCorrect++;
                                             }
                                             else {
                                                 Object.assign(result.style, css);
@@ -217,6 +316,9 @@ btnStart.addEventListener("click", function () {
                                                         return;
                                                     }
                                                 });
+                                                strike.push(countStrike);
+                                                countStrike = 0;
+                                                countIncorrect++;
                                             }
                                         }
 
@@ -230,6 +332,8 @@ btnStart.addEventListener("click", function () {
                                 }
                                 canClick = false;
                                 cancelAnimationFrame(currentAnimation);
+                                updateStreakBar(countStrike);
+                                console.log(strike);
                             }
                         }
                     });
@@ -247,4 +351,7 @@ btnStart.addEventListener("click", function () {
             numberCountdown.innerText = "Go!";
         }
     }, 1000);
-});
+}
+
+
+btnStart.addEventListener("click", handleQuizizz);
